ced4755edfa22d9479cd033e6974857df979bc06
sedat_3278@hotmail.com <sedat_3278@hotmail.com>
2018-05-05 14:38:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {rihanna: "diamonds"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+  return Object.assign(playlist, {[artistName]: songTitle});
 }
